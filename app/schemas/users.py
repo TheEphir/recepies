a1279f6a1aa976bac2059df6851a10ea4db350b5
Максим Лучко <maksimus247@gmail.com>
2025-10-27 12:33:33 +0200

    made validations scheme for users

---
+++ app/schemas/users.py
@@ -0,0 +1,24 @@
+import datetime
+from uuid import UUID
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    password: str
+    
+
+class UserResponse(UserBase):
+    id: UUID
+    created_at: datetime
+    is_active: bool = True
+    is_admin: bool = False
+    
+    class Config:
+        from_atttbutes = True # for work with SQLAlchemy
