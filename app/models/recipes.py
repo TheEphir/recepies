31b2147e92a5149ebc10e0165899050668962719
Максим Лучко <maksimus247@gmail.com>
2025-10-27 13:35:45 +0200

    made psql model for recipes

---
+++ app/models/recipes.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, String, Integer, Boolean, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID, JSONB
+from sqlalchemy.sql import func
+import uuid
+
+class Recipe():
+    __tablename__ = "recipes"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
+    title = Column(String(200), nullable=False, index=True)
+    ingredients = Column(JSONB, nullable=False)
+    instructions = Column(String, nullable=False)
+    category = Column(String(100), nullable=True, index=True)
+    prep_time = Column(Integer, nullable=True)
+    cook_time = Column(Integer, nullable=True)
+    servings = Column(Integer, nullable=True)
+    image_url = Column(String(255), nullable=True)
+    is_public = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+
+    __table_args__ = (
+        {"postgresql_include": ["title", "category", "user_id"]} # making indexes
+    )
