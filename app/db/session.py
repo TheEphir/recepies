3ecd11a38b213b29c21452a1a0f611c1ad5a6c3e
TheEphir <maksimus247@gmail.com>
2025-10-28 14:07:47 +0200

    preparations for migrations

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from config.settings import settings
 
-DATABASE_URL = settings.DATABASE_URL  # Например, "postgresql+asyncpg://user:password@localhost:5432/my_api"
+DATABASE_URL = settings.DATABASE_URL  # "postgresql+asyncpg://user:password@localhost:5432/my_api"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
